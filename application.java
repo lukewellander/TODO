98567c749f97d14bf0fcf3290203f0c05e845f31
lwellander18 <lwellander18@georgefox.edu>
2021-09-15 11:51:45 -0700

    made a mess

--- application.java
+++ application.java
@@ -1,14 +1,52 @@
 package todo;
 
 import javax.swing.*;
+import java.awt.event.*;
 
-public class application {
-    public static void main(String[] args) {
+public class application implements ActionListener{
+
+    runApp() {
         JFrame frame = new JFrame("//TODO");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
-        JButton button = new JButton("Press");
-        frame.getContentPane().add(button);
-        frame.setVisible(true);  
+
+        JTextField newTaskNameField = new JTextField("New Task");
+        newTaskNameField.setBounds(5,5,250,30);
+
+        JButton button = new JButton("Create");
+        button.setBounds(260,5,100,30);
+
+        button.addActionListener(this);
+
+        frame.add(newTaskNameField);
+        frame.add(button);
+        
+        frame.setSize(380,600);
+        frame.setLayout(null);
+        frame.setVisible(true); 
+    }
+
+    public static void main(String[] args) {
+        new runApp();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    private void createNewTask(String name) {
+
+    }
+
+    public class Task {
+        Task subTasks[];
+        boolean done = false;
+
+        private void check() {
+            done = true;
+        }
+
+        private boolean done() {
+            return done;
+        }
     }
 }
